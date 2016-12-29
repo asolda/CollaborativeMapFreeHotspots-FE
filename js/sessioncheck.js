5e14270296527301257f1e33b328d0e71b899714
Alfredo Di Nuccio <Alfredo Di Nuccio>
2016-12-29 16:46:46 +0100

    Aggiunti sessioncheck e getcookie, sistemato script caricamento index e login.js

--- js/sessioncheck.js
+++ js/sessioncheck.js
@@ -1,23 +1,23 @@
- function login(email, password){
+ function sessioncheck(cookie){
           $.ajax({
-            type: 'POST',
-            url: 'http://127.0.0.1:8080/login/',
-            data: "email="+email+"&password="+password,
+            type: 'GET',
+            url: 'http://127.0.0.1:8080/sessioncheck/',
+            data: "cookie="+cookie,
             contentType: "application/x-www-form-urlencoded",
             success: function(data) {
               try {
                 var ret = jQuery.parseJSON(JSON.stringify(data));
                 if(ret.status==0){
                     $('#result').append(ret.message + '</br>');
-					return 1; //login ok
+					return id_utente; //sessione esistente
                 }else if(ret.status==1){
                     $('#result').append(ret.message + '</br>');
-					return 0; //email e/o password errati
+					return ""; //sessione non esistente
                 }
                 
                 
               } catch (err) {
-                alert('Errore nel login: ' + ret.message);
+                alert('Errore nella richiesta di controllo sessione: ' + ret.message);
               }
             },
             error: function(xhr, status, error) {
