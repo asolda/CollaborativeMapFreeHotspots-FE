564c3c76bdfbb843e0c2d3b55ed13784e76a869a
Gianmarco Beato <Gianmarco Beato>
2017-01-01 23:35:33 +0100

    Vari ritocchi grafici

--- js/main.js
+++ js/main.js
@@ -62,7 +62,10 @@ function createLoggedHome(){
                 '<a id="show-editpassword" class="mdl-navigation__link drawerlink" href="#"><span class="space"/><i class="material-icons">vpn_key</i><span class="space"/>Modifica Password</a>' +
                 '<a id="show-deleteaccount" class="mdl-navigation__link drawerlink" href="#"><span class="space"/><i class="material-icons">delete</i><span class="space"/>Elimina Account</a>' +
               '</nav>' +
-           '</div>';
+           '</div>' +
+           '<div class="mdl-tooltip mdl-tooltip--large" data-mdl-for="closeDrawer">' +
+                'Chiudi men&ugrave' +
+              '</div>' ;
     
     // Crea l'action button per inserire una mappa 
     actionbtn = '<div id="left-actionbtn">'+
