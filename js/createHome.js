a449d74fb55a799635dda953316e8ae9d6b3d140
Alfredo Di Nuccio <Alfredo Di Nuccio>
2017-01-04 16:55:27 +0100

    Sistemato e ultimato script modulare per il caricamento della pagina iniziale in base all'esistenza o meno della sessione

---
+++ js/createHome.js
@@ -0,0 +1,21 @@
+function createHome() {
+	var cookie = getCookie("actoken32");
+			if (strcmp(cookie, "") != 0) { // getcookie restituisce il valore del cookie, se esiste, o la stringa vuota, se non esiste; dopodiché, si controlla se tale cookie esiste confrontando il valore restituito con la stringa vuota
+				var id_utente = sessionCheck(); // sessioncheck deve restituire l'id dell'utente da passare a createloggedhome o 0 se la sessione non esiste nel DB
+				if (id_utente != 0) {
+					// SE IL COOKIE E LA SESSIONE ESISTONO E GLI ID CORRISPONDONO
+					//Inizializza la home per l'utente loggato
+					createLoggedHome(id_utente);
+				}
+				else {
+					// SE LA SESSIONE NON ESISTE
+					//Inizializza la home per l'utente non loggato
+					createNotLoggedHome();
+				}
+			}
+			else {
+				//SE IL COOKIE NON ESISTE
+				//Inizializza la home per l'utente non loggato
+				createNotLoggedHome();
+			}
+}
