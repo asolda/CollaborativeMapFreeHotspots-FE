6f7ec65416ed3fadfbcbfc62bfd0dd7abe82364f
Alfredo Di Nuccio <Alfredo Di Nuccio>
2017-01-06 19:43:01 +0100

    Fixato e terminato controllo sessione pre-caricamento home, manca il controllo del cookie in locale

--- js/createHome.js
+++ js/createHome.js
@@ -1,21 +1,24 @@
 ﻿function createHome() {
-	var cookie = getCookie("actoken32");
+	
+	var cookie = getCookie('actoken32');
+	alert('cookie value: '+cookie)
 			if (strcmp(cookie, "") != 0) { // getcookie restituisce il valore del cookie, se esiste, o la stringa vuota, se non esiste; dopodiché, si controlla se tale cookie esiste confrontando il valore restituito con la stringa vuota
-				var id_utente = sessionCheck(); // sessioncheck deve restituire l'id dell'utente da passare a createloggedhome o 0 se la sessione non esiste nel DB
-				if (id_utente != 0) {
-					// SE IL COOKIE E LA SESSIONE ESISTONO E GLI ID CORRISPONDONO
-					//Inizializza la home per l'utente loggato
-					createLoggedHome(id_utente);
-                                        //chiude il menù laterale al click sull'icona <<
-                                        document.getElementById("closeDrawer").addEventListener("click", function(){
-                                          $( 'div[class^="mdl-layout__obfuscator"]' ).trigger( "click" );
-                                        });
-				}
-				else {
-					// SE LA SESSIONE NON ESISTE
-					//Inizializza la home per l'utente non loggato
-					createNotLoggedHome();
-				}
+				sessionCheck(function(ret) { // sessioncheck deve restituire l'id dell'utente da passare a createloggedhome o 0 se la sessione non esiste nel DB
+						var id_utente = ret;
+						if (id_utente != 0) {
+							// SE IL COOKIE E LA SESSIONE ESISTONO E GLI ID CORRISPONDONO
+							//Inizializza la home per l'utente loggato
+							createLoggedHome();
+							//chiude il menù laterale al click sull'icona <<
+							document.getElementById("closeDrawer").addEventListener("click", function(){
+								$( 'div[class^="mdl-layout__obfuscator"]' ).trigger( "click" );
+							});
+						} else {
+							// SE LA SESSIONE NON ESISTE
+							//Inizializza la home per l'utente non loggato
+							createNotLoggedHome();
+						}
+				});
 			}
 			else {
 				//SE IL COOKIE NON ESISTE
