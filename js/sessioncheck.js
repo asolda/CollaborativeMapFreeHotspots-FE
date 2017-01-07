b01339d9f1da827d9b7d58fe721b599dce818600
Alfredo Di Nuccio <Alfredo Di Nuccio>
2017-01-07 16:11:30 +0100

    Aggiunto file mancante nel precedente commit

--- js/sessioncheck.js
+++ js/sessioncheck.js
@@ -1,18 +1,23 @@
- function sessionCheck(){
+ function sessionCheck(check){
           $.ajax({
             type: 'GET',
             url: 'http://127.0.0.1:8080/session/check/',
+			crossDomain: true,
+			xhrFields: {
+				withCredentials: true
+			},
             success: function(data) {
               try {
                 var ret = data;
                 if(ret.status == 0){
-                    $('#result').append(ret.message + '</br>');
-					var id_utente = ret.message.user;
-					return id_utente; //sessione esistente
+					//sessione esistente
+                    //$('#result').append(ret.message + '</br>');
+					check(ret.message.user);
                 }
 				else if(ret.status == 1){
-                    $('#result').append(ret.message + '</br>');
-					return 0; //sessione non esistente
+					//sessione non esistente
+                    //$('#result').append(ret.message + '</br>');
+					check(ret.message);
                 }
                 
                 
