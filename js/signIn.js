be05b820747578a942275f49af4ee177228eb36f
martaceliento <martaceliento96@gmail.com>
2017-01-10 13:48:18 +0100

    Modificata la funzione di login

--- js/signIn.js
+++ js/signIn.js
@@ -1,4 +1,11 @@
- function signIn(email, password, onSignInTermination){
+ function signIn(email, password, onclose){
+     if((email.length==0 || email==null) && (password.length==0 || password==null)){
+           onclose(false,'CAMPI_EMAIL_PASSWORD_NON_COMPILATI'); 
+     }else if((email.length==0 || email==null)){
+           onclose(false,'CAMP0_EMAIL_NON_COMPILATO');
+     }else if((password.length==0 || password==null)){
+           onclose(false,'CAMP0_PASSWORD_NON_COMPILATO');
+     }else{     
           $.ajax({
             type: 'POST',
             url: 'http://127.0.0.1:8080/user/login/',
@@ -12,14 +19,23 @@
               try {
                 var ret = data;
                 if(ret.status==0){
-					//login ok
-                    //$('#result').append(ret.message + '</br>');
-					onSignInTermination('LOGIN_OK', ret.message.user);
+					onclose(true,ret.message);
                 }
 				else if(ret.status==1){
-					//errore login
-                    //$('#result').append(ret.message + '</br>');
-					onSignInTermination(ret.message, null);
+					 $('#result').append(ret.message + '</br>');
+                     if(strcmp(ret.message,'CANNOT_LOGIN')==0){
+                        onclose(false,'CANNOT_LOGIN');
+                     }else if(strcmp(ret.message,'ERROR_GENERATING_SESSION')==0){
+                        onclose(false,'ERROR_GENERATING_SESSION');
+                     }else if(strcmp(ret.message,'ERROR_EMAIL_PASSWORD')==0){
+                        onclose(false,'ERROR_EMAIL_PASSWORD');
+                     }else if(strcmp(ret.message,'ERROR_EMAIL')==0){
+                        onclose(false,'ERROR_EMAIL');
+                     }else if(strcmp(ret.message,'ERROR_PASSWORD')==0){
+                        onclose(false,'ERROR_PASSWORD');
+                     }else if(strcmp(ret.message,'ERROR_CREDENTIALS')==0){
+                        onclose(false,'ERROR_CREDENTIALS');
+                     }                     
                 }
 
 
@@ -32,6 +48,7 @@
             }
           });
          }
+ }
          
 /** Funzione per effetuare il logout **/
 function signOut(){
