f5297187ba0dfab08747fbb2694ff0a21ff90233
martaceliento <martaceliento96@gmail.com>
2017-01-22 16:53:38 +0100

    Aggiunta funzione eliminazione account

---
+++ js/deleteaccount.js
@@ -0,0 +1,35 @@
+function deleteAccount(password,onclose){
+    if(password.length==0 || password==null){
+           onclose(false,"EMPTY_FIELD");
+    }else{ 
+            $.ajax({
+            type: 'POST',
+            url: 'http://127.0.0.1:8080/user/delete/request/',
+            data: "password="+password+"&frontend_url="+window.location.href+"", 
+            contentType: "application/x-www-form-urlencoded",
+            crossDomain: true,
+            xhrFields: {
+                withCredentials: true
+            },
+            success: function(data){
+              try {
+                var ret = data;
+                if(ret.status==0){
+                    onclose(true,ret.message);
+                }else if(ret.status==1){
+                    if(strcmp(ret.message,'ERROR_SESSION')){
+                      onclose(false,'ERROR_SESSION');   
+                    }else if(strcmp(ret.message,'ERROR_PASSWORD')){
+                      onclose(false,'ERROR_PASSWORD');   
+                    }
+                }
+              }catch (err){
+                 alert('Errore nel eliminazione account: ' + ret.message);
+              }
+            },
+            error: function(xhr, status, error) {
+               console.log('Error: ' + error.message);
+              }
+            });
+    }                    
+}   
